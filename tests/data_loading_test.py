dbaed715b386a8fe48f58738037302956819fb16
Wiktor Lazarski <wjlazarski@gmail.com>
2020-12-01 21:47:57 +0100

    Tests for Dataset class and small project reorganization

--- tests/data_loading_test.py
+++ tests/data_loading_test.py
@@ -1,15 +1,34 @@
+import pytest
 import scripts.data_loading as dl
 import torch
 
 
-def test_load_dataset() -> None:
+@pytest.fixture
+def coco_dataset() -> dl.CocoCaptions:
+    dset_type = dl.DatasetType.VALIDATION
+    return dl.CocoCaptions(dset_paths=dl.TRAINING_DATASET_PATHS[dset_type])
+
+
+def test_coco_dataset_len(coco_dataset: dl.CocoCaptions) -> None:
     # given
-    expected_size = 5_000
-    expected_out_tensor_dim = torch.Size([3, 224, 224])
+    expected_result = 25_014
 
     # when
-    dataset = dl.load_dataset(service.DatasetType.Validation, vgg_preprocessed=True)
+    result = len(coco_dataset)
 
     # then
-    assert len(dataset) == expected_size
-    assert dataset[0][0].shape == expected_out_tensor_dim
+    assert result == expected_result
+
+
+def test_coco_dataset_sorted_by_captions(coco_dataset: dl.CocoCaptions) -> None:
+    # given
+    examine_first = 500
+
+    # when
+    lengths = []
+    for i in range(examine_first):
+        _, caption = coco_dataset[i]
+        lengths.append(len(caption.split()))
+
+    # then
+    assert lengths == sorted(lengths, reverse=True)
