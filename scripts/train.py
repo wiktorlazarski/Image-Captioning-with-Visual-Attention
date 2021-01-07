ccb438b16f5c588329bb9ff4c9b0c916837db263
Wiktor Lazarski <wjlazarski@gmail.com>
2021-01-07 18:18:33 +0100

    Implemented early stopping on BLEU-4.

--- scripts/train.py
+++ scripts/train.py
@@ -59,6 +59,7 @@ class ImageCaptioningTrainer:
         image_pipeline: transforms.transforms = dp.VGGNET_PREPROCESSING_PIPELINE,
         caption_pipeline: dp.TextPipeline = dp.TextPipeline(),
         checkpoint_dir: str = os.path.join(os.environ["TORCH_HOME"], "checkpoints"),
+        best_models_dir: str = os.path.join(os.environ["TORCH_HOME"], "best_models")
     ):
         self.coco_train = dl.CocoCaptions(
             dset_paths=coco_train_paths,
@@ -74,6 +75,10 @@ class ImageCaptioningTrainer:
         if not os.path.exists(self.checkpoint_dir):
             os.mkdir(self.checkpoint_dir)
 
+        self.best_models_dir = best_models_dir
+        if not os.path.exists(self.best_models_dir):
+            os.mkdir(self.best_models_dir)
+
         self.validator = ev.CocoValidator(coco_val_paths, self.coco_train.target_transform.vocabulary)
 
     def train(
@@ -87,26 +92,24 @@ class ImageCaptioningTrainer:
         decoder_dim: int,
         attention_dim: int,
         dropout: float,
+        maxlen: int,
+        patience: int,
         checkpoint_path: Optional[str] = None,
-    ) -> None:
+    ) -> float:
         checkpoint = torch.load(checkpoint_path) if checkpoint_path is not None else None
+
+        best_bleu = 0.0
+        epochs_without_improvement = 0
+
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        logging.info(f"Training on device {torch.cuda.get_device_name(device.index)}")
 
         with ImageCaptioningTrainer.tensorboard(comment=f"_batch={batch_size}_lr={lr}_lambda={loss_lambda}_dropout={dropout}") as tb:
             data_loader = dl.CocoLoader(self.coco_train, batch_size, math.ceil(os.cpu_count() / 2))
 
-            decoder = model.LSTMDecoder(
-                num_embeddings=self.num_embeddings,
-                embedding_dim=embedding_dim,
-                encoder_dim=self.encoder_dim,
-                decoder_dim=decoder_dim,
-                attention_dim=attention_dim,
-                dropout=dropout,
-            )
+            decoder = model.LSTMDecoder(self.num_embeddings, embedding_dim, self.encoder_dim, decoder_dim, attention_dim, dropout)
             if checkpoint is not None:
                 decoder.load_state_dict(checkpoint["decoder"])
-
-            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-            logging.info(f"Training on device {torch.cuda.get_device_name(device.index)}")
 
             self.encoder.to(device)
             decoder.to(device)
@@ -120,12 +123,14 @@ class ImageCaptioningTrainer:
                 ignore_index=self.coco_train.target_transform.vocabulary.word2idx("<PAD>"),
             ).to(device)
 
+            # Training loop
             start_epoch = 1 if checkpoint is None else checkpoint["epoch"]
             for epoch in range(start_epoch, start_epoch + num_epochs):
                 decoder.train()
                 cost = 0.0
                 running_loss = 0.0
 
+                # Train steps
                 for step, batch in enumerate(data_loader, 1):
                     images, captions = batch[0].to(device), batch[1].to(device)
 
@@ -147,6 +152,7 @@ class ImageCaptioningTrainer:
                     cost += loss.item()
                     running_loss += loss.item()
 
+                    # Print loss every n-th step
                     every_step = 100
                     if not step % every_step:
                         avg_loss = running_loss / every_step
@@ -154,59 +160,39 @@ class ImageCaptioningTrainer:
                         logging.info(f"Epoch {epoch} Step {step}/{len(data_loader)} => {avg_loss: .4f}")
                         tb.add_scalar(f"loss_lambda={loss_lambda}", avg_loss, step + (epoch - 1) * len(data_loader))
 
-                self._save_checkpoint(
-                    epoch=epoch,
-                    decoder_state=decoder.state_dict(),
-                    optim_state=optimizer.state_dict(),
-                    lr=lr,
-                    dropout=dropout,
-                    loss_lambda=loss_lambda,
-                )
+                self._save_checkpoint(epoch, decoder.state_dict(), optimizer.state_dict(), lr, dropout, loss_lambda)
 
-                bleu_4 = self.validator.validate(self.encoder, decoder, device)
-                logging.info(f"After Epoch {epoch} BLEU-4 => {bleu_4}")
+                current_bleu4 = self.validator.validate(self.encoder, decoder, device)
+                logging.info(f"After Epoch {epoch} BLEU-4 => {current_bleu4}")
 
-                self._save_tensorboard_data(
-                    epoch=epoch,
-                    cost=cost / len(data_loader),
-                    bleu=bleu_4,
-                    loss_lambda=loss_lambda,
-                    decoder=decoder,
-                    writer=tb,
-                )
+                self._save_run(epoch, cost / len(data_loader), current_bleu4, loss_lambda, decoder, tb)
+
+                # Early stopping on BLEU-4 metric
+                if current_bleu4 > best_bleu:
+                    best_bleu = current_bleu4
+                    epochs_without_improvement = 0
+
+                    self._save_best_model(embedding_dim, decoder_dim, attention_dim, decoder)
+                else:
+                    epochs_without_improvement += 1
+                    if epochs_without_improvement == patience:
+                        break
 
                 self.coco_train.shuffle(subset_len=1000)
 
-    def _save_checkpoint(
-        self,
-        epoch: int,
-        decoder_state: dict,
-        optim_state: dict,
-        lr: float,
-        dropout: float,
-        loss_lambda: float,
-    ) -> None:
+        return best_bleu
+
+    def _save_checkpoint(self, epoch: int, decoder_state: dict, optim_state: dict, lr: float, dropout: float, loss_lambda: float) -> None:
         checkpoint = {
             "epoch": epoch,
             "decoder": decoder_state,
             "optimizer": optim_state,
         }
 
-        checkpoint_path = os.path.join(
-            self.checkpoint_dir,
-            f"decoder_lr_{lr}_dropout_{dropout}_lambda_{loss_lambda}.pth",
-        )
+        checkpoint_path = os.path.join(self.checkpoint_dir, f"decoder_lr_{lr}_dropout_{dropout}_lambda_{loss_lambda}.pth")
         torch.save(checkpoint, checkpoint_path)
 
-    def _save_tensorboard_data(
-        self,
-        epoch: int,
-        cost: float,
-        bleu: float,
-        loss_lambda: float,
-        decoder: model.LSTMDecoder,
-        writer: tb.SummaryWriter,
-    ) -> None:
+    def _save_run(self, epoch: int, cost: float, bleu: float, loss_lambda: float, decoder: model.LSTMDecoder, writer: tb.SummaryWriter) -> None:
         writer.add_scalar(f"BLEU-4", bleu, epoch)
         writer.add_scalar(f"cost_lambda={loss_lambda}", cost, epoch)
 
@@ -214,16 +200,23 @@ class ImageCaptioningTrainer:
             writer.add_histogram(name, weight, epoch)
             writer.add_histogram(f"{name}.grad", weight.grad, epoch)
 
+    def _save_best_model(self, embedding_dim: int, decoder_dim: int, attention_dim: int, decoder: model.LSTMDecoder) -> None:
+        output_path = os.path.join(self.best_models_dir, f"best_model_e{embedding_dim}_a{attention_dim}_d{decoder_dim}.pth")
+
+        torch.save(decoder.state_dict(), output_path)
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Image Captioning with Visual Attention training process")
-    parser.add_argument("--num_epochs", type=int, required=True, help="Number of epochs to perform in training process")
-    parser.add_argument("--batch_size", type=int, required=True, help="Batch size")
-    parser.add_argument("--lr", type=float, required=True, help="Learning rate. If checkpoint passed then learning rate will be loaded from state_dict")
-    parser.add_argument("--loss_lambda", type=float, required=True, help="Value of hyperparameter lambda from loss function")
-    parser.add_argument("--embedding_dim", type=int, required=True, help="Word embedding dimmension")
-    parser.add_argument("--decoder_dim", type=int, required=True, help="LSTM layer dimmension")
-    parser.add_argument("--attention_dim", type=int, required=True, help="Additive attendion dimmension")
+    parser.add_argument("--num_epochs", default=1, type=int, help="Number of epochs to perform in training process")
+    parser.add_argument("--batch_size", default=64, type=int, help="Batch size")
+    parser.add_argument("--lr", default=0.01, type=float, help="Learning rate. If checkpoint passed then learning rate will be loaded from state_dict")
+    parser.add_argument("--loss_lambda", default=1.0, type=float, help="Value of hyperparameter lambda from loss function")
+    parser.add_argument("--embedding_dim", default=128, type=int, help="Word embedding dimmension")
+    parser.add_argument("--decoder_dim", default=512, type=int, help="LSTM layer dimmension")
+    parser.add_argument("--attention_dim", default=512, type=int, help="Additive attendion dimmension")
+    parser.add_argument("--maxlen", default=100, type=int, help="Maximum caption length in words which can be produced during validation step if <EOS> not met")
+    parser.add_argument("--patience", default=10, type=int, help="Number of epochs which need to pass while doing early stopping on BLEU-4 metric.")
     parser.add_argument("--dropout", default=0.5, type=float, help="Dropout regularization")
     parser.add_argument("--checkpoint_path", type=str, help="Checkpoint path")
 
@@ -231,16 +224,12 @@ def parse_args() -> argparse.Namespace:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s %(levelname)s: %(message)s",
-        datefmt="%d/%m/%Y %H:%M",
-    )
-
-    trainer = ImageCaptioningTrainer()
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s", datefmt="%d/%m/%Y %H:%M")
 
     args = parse_args()
-    trainer.train(
+
+    trainer = ImageCaptioningTrainer()
+    final_bleu = trainer.train(
         num_epochs=args.num_epochs,
         batch_size=args.batch_size,
         lr=args.lr,
@@ -249,5 +238,9 @@ if __name__ == "__main__":
         decoder_dim=args.decoder_dim,
         attention_dim=args.attention_dim,
         dropout=args.dropout,
+        maxlen=args.maxlen,
+        patience=args.patience,
         checkpoint_path=args.checkpoint_path,
     )
+
+    logging.info(f"Training FINISHED. Final validation BLEU-4 = {final_bleu: .4f}.")
