c6a62f7ec342fed1d802eb93e69b270a6c750289
Wiktor Lazarski <wjlazarski@gmail.com>
2021-02-07 16:51:49 +0100

    Import reorganization

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -10,7 +10,7 @@ from PIL import Image
 from pycocotools.coco import COCO
 from torchvision import datasets as dset
 
-import scripts.data_processing as dp
+import data_processing as dp
 
 
 class DatasetType(Enum):
