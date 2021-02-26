2320e641d782d829d2143690d34df37cc7ba1fb5
Wiktor Lazarski <wjlazarski@gmail.com>
2021-02-26 22:06:08 +0100

    code & project refactorization

--- API/run.sh
+++ API/run.sh
@@ -2,6 +2,6 @@
 
 IP_ADDRESS=$(hostname -I | sed s/\ .*//)
 
-echo "---> Server IPv4 address in private network is $IP_ADDRESS <---"
+echo "---> Server Network address in private network is $IP_ADDRESS:1234 <---"
 
 gunicorn -w 1 -b 0.0.0.0:1234 --log-level ERROR API.server:app
